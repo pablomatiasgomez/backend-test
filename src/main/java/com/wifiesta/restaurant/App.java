94a97d0618cd906e2fd19a140d94ab449df95d8d
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2016-11-15 20:30:22 -0300

    startup

---
+++ src/main/java/com/wifiesta/restaurant/App.java
@@ -0,0 +1,7 @@
+package com.wifiesta.restaurant;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/wifiesta/restaurant/AppTest.java
@@ -0,0 +1,8 @@
+package com.wifiesta.restaurant;
+
+import junit.framework.TestCase;
+
+public class AppTest
+    extends TestCase {
+
+}
